ee3fd0349c1cb935f188963319a0b6263e4195bb
Jithin Ramesh <109571606+jithinramesh@users.noreply.github.com>
2022-10-10 18:24:41 +0530

    hgv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NewsComponent } from './news/news.component';
 
 const link:Routes=[
-  {path:" ",component:NewsComponent}
+  {path:"",component:NewsComponent}
 ]
 
 @NgModule({
@@ -21,6 +22,7 @@ const link:Routes=[
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(link)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
